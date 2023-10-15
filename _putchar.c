c9ec94fc112cf862e456be04cf0855f012aa8a9d
Josh19744 <olotujoshuaolaoluwa@gmail.com>
2023-10-15 09:12:22 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ _putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdio
+ * @c: the character to print
+ * Return: on success 1
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1))
+}
