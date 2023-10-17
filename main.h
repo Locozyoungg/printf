b47d73162470595964b952d371387b0069417683
Josh19744 <olotujoshuaolaoluwa@gmail.com>
2023-10-17 02:32:00 -0700

    updated

--- main.h
+++ main.h
@@ -4,6 +4,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+#define S_LONG 2
+#define S_SHORT 1
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
@@ -56,5 +68,12 @@ char buffer[],
 int flags, int width, int precision, int size);
 int _putchar(char c);
 int _puts(char *str);
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
 
 #endif
