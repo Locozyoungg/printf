9b8c86ac1d382afbfa5a2b2fbf245f6b21292927
Locozyoungg <collaustin27@gmail.com>
2023-10-17 09:14:16 -0700

    majorfix

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
-#include <limits.h>
 #include <stdio.h>
+#include <limits.h>
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
 int main(void)
 {
     int len;
@@ -38,7 +33,6 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
     return (0);
 }
+
