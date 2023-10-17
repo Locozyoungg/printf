0bf00f5599845e88c87b54947e0ae36cf0ecba5c
Josh19744 <olotujoshuaolaoluwa@gmail.com>
2023-10-17 02:21:52 -0700

    updated

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdar
-/* Funtions to print chars and strings */
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -10,7 +12,6 @@ int print_string(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Functions to print numbers */
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
@@ -27,26 +28,21 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
-/* Funcion to print memory address */
-int print_pointer(va_list types, char buffer[],
+int_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
-/*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-/*Function to print a string in rot 13*/
+
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-/* width handler */
+
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
@@ -58,15 +54,7 @@ int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 int flags, int width, int precision, int size);
-
-
-=======
 int _putchar(char c);
 int _puts(char *str);
-<<<<<<< HEAD
->>>>>>> 21f62d8b020213740a655be6713fb2228931fa72
-=======
-int count_digits(int n);
->>>>>>> 7ad2f0a2437750e3c8df79334e92b2c318ff10c4
 
 #endif
